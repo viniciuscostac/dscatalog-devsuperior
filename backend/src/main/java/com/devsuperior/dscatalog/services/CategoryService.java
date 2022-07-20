1940b3c3e0875df142d73418705d1125802e1d1b
Vinicius Costa <vinicius.ccosta@outlook.com>
2022-07-19 23:59:59 -0300

    Database Seed

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -17,6 +18,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true) //Coloco essa anotação caso eu queira que a operação do método tenha transação com o banco
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
